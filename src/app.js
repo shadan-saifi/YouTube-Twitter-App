50dd61a6f98bee21c3ddd3bbb1f3ec4309dfb6dd
Shadan Saifi <shadansaif99@gmail.com>
2024-03-11 11:02:21 +0530

    add

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import connectDB from './db/index.js'
+// import connectDB from './db/index.js'
 
 const app = express()
 
