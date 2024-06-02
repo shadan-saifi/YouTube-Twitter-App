b7136f811660a8e423cc4fd9e26e5033188829ff
Shadan Saifi <shadansaif99@gmail.com>
2024-06-02 22:07:17 +0530

    a

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,14 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB=async()=>{
     try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            bufferCommands: false, // Disable buffering to set custom timeout
+            bufferTimeoutMS: 60000 // Set the timeout to 60 seconds
+          })
         console.log(`\n mongodb connected!! DBHos:${connectionInstance.connection.host}`);
     } catch (error) {
      console.log("MongoDB connection error:",error);
@@ -13,4 +20,17 @@ const connectDB=async()=>{
 }
 
 export default connectDB
+
+// mongoose.connect('mongodb://localhost:27017/mydatabase', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+//   bufferCommands: false, // Disable buffering to set custom timeout
+//   bufferTimeoutMS: 60000 // Set the timeout to 60 seconds
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch((err) => {
+//   console.error('Failed to connect to MongoDB', err);
+// });
 
